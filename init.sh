c23c4563819cf7079ef898b2755942eac72ed80b
Juan Miguel <136333192+JuanMi-CG@users.noreply.github.com>
2025-02-26 19:53:02 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,17 +5,17 @@
 BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 echo "Iniciando servicio de base de datos..."
-sudo "$BASE_DIR/db.sh"
+sudo "$BASE_DIR/db.sh" > db.log 2>&1 
 # Esperar a que la base de datos esté lista (ajustar el tiempo si es necesario)
 sleep 15
 
 echo "Iniciando backend (Flask)..."
-sudo "$BASE_DIR/backend.sh" &
+sudo "$BASE_DIR/backend.sh" > backend.log 2>&1 &
 
 echo "Iniciando frontend (Vue.js)..."
-sudo "$BASE_DIR/frontend.sh" &
+sudo "$BASE_DIR/frontend.sh" > frontend.log 2>&1 &
 
 echo "Iniciando Jupyter Notebook..."
-sudo "$BASE_DIR/jupyter.sh" &
+sudo "$BASE_DIR/jupyter.sh" > jupyter.log 2>&1 &
 
 echo "Todos los servicios han sido iniciados."
