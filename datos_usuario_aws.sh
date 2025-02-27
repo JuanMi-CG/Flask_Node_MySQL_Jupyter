9bec3641b17ca1e263d9841c6f3c863a12251cda
Juan Miguel <136333192+JuanMi-CG@users.noreply.github.com>
2025-02-27 18:31:23 +0100

    Update datos_usuario_aws.sh

--- datos_usuario_aws.sh
+++ datos_usuario_aws.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-# Actualizar el sistema
-sudo yum update -y
+#-----------------------------------------------
+# Configuración de reserva de memoria para ECS y límite de zram
+#-----------------------------------------------
 
-# Instalar Git
+# Reservar 256 MB para procesos críticos (para el agente ECS u otros servicios vitales)
+sudo mkdir -p /etc/ecs
+echo "ECS_RESERVED_MEMORY=256" | sudo tee /etc/ecs/ecs.config
+
+# Configurar zram para limitar su uso a 800MB
+sudo mkdir -p /etc/systemd/zram-generator.conf.d
+echo "[zram0]" | sudo tee /etc/systemd/zram-generator.conf.d/limit.conf
+echo "max_size = 800M" | sudo tee -a /etc/systemd/zram-generator.conf.d/limit.conf
+
+#-----------------------------------------------
+# Actualizar el sistema e instalar Git
+#-----------------------------------------------
+sudo yum update -y
 sudo yum install -y git
 
+#-----------------------------------------------
 # Crear el directorio del proyecto y clonar el repositorio
+#-----------------------------------------------
 mkdir -p /home/ec2-user/ERP
 cd /home/ec2-user/ERP
 git clone https://github.com/JuanMi-CG/Flask_Node_MySQL_Jupyter .
@@ -16,4 +31,4 @@ sudo chown -R ec2-user:ec2-user /home/ec2-user/ERP
 # Dar permisos de ejecución al script de inicialización y ejecutarlo
 cd /home/ec2-user/ERP
 chmod +x *.sh
-./init.sh
+# sudo ./init.sh
